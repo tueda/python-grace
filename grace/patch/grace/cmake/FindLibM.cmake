599d6f0594c283456e4399c9dc5afc12565526d9
Takahiro Ueda <tueda@st.seikei.ac.jp>
2021-06-30 20:06:23 +0900

    build: disable a debug message

--- grace/patch/grace/cmake/FindLibM.cmake
+++ grace/patch/grace/cmake/FindLibM.cmake
@@ -15,7 +15,7 @@ endif()
 
 find_package_handle_standard_args(LibM REQUIRED_VARS LIBM_OK)
 
-message("LIBM_OK=${LIBM_OK}")
+# message("LIBM_OK=${LIBM_OK}")
 
 if(LIBM_OK AND NOT TARGET LibM::LibM)
   add_library(LibM::LibM INTERFACE IMPORTED)
